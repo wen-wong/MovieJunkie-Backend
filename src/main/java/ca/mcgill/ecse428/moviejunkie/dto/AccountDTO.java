0995b48d73c49dcea59e08ef4f496223e43e58c4
Hana Gustyn <hana.gustyn@mail.mcgill.ca>
2023-02-11 16:21:26 -0500

    Added AccountDTO

--- src/main/java/ca/mcgill/ecse428/moviejunkie/dto/AccountDTO.java
+++ src/main/java/ca/mcgill/ecse428/moviejunkie/dto/AccountDTO.java
@@ -1,4 +1,71 @@
 package ca.mcgill.ecse428.moviejunkie.dto;
 
+import ca.mcgill.ecse428.moviejunkie.model.Account;
+
 public class AccountDTO {
+  private int accountId;
+  private String firstName;
+  private String lastName;
+  private String username;
+  private String password;
+  private String email;
+
+  public AccountDTO(){}
+
+  public AccountDTO(int accountId, String firstName, String lastName, String username, String password, String email) {
+    this.accountId = accountId;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.username = username;
+    this.password = password;
+    this.email = email;
+  }
+
+  public int getAccountId() {
+    return accountId;
+  }
+
+  public void setAccountId(int accountId) {
+    this.accountId = accountId;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
 }
