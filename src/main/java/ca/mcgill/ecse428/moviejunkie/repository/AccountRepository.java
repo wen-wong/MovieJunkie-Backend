474f71d118d150986294a2747d40b8eb28b85072
Victoria Sanchez <victoriasofia@Victorias-MacBook-Air.local>
2023-02-11 18:38:45 -0500

    getting database to work' '

--- src/main/java/ca/mcgill/ecse428/moviejunkie/repository/AccountRepository.java
+++ src/main/java/ca/mcgill/ecse428/moviejunkie/repository/AccountRepository.java
@@ -2,6 +2,6 @@ package ca.mcgill.ecse428.moviejunkie.repository;
 import ca.mcgill.ecse428.moviejunkie.model.Account;
 import org.springframework.data.repository.CrudRepository;
 public interface AccountRepository extends CrudRepository<Account, Integer>{
-    Account findAccountByID(int id);
+    //Account findAccountByID(int id);
     Account findAccountByUsername(String username);
 }
