5af5cd6b2c779a5cbabed042c2a84a3c206a4ab1
Matthew Litwiller <matthew.litwiller@mail.mcgill.ca>
2023-02-10 21:49:32 -0500

    Setup model class and repository interface for account
    The code has not been tested as I still am unclear on how to access the database and see if we are able to persist information using the model

--- src/main/java/ca/mcgill/ecse428/moviejunkie/model/Account.java
+++ src/main/java/ca/mcgill/ecse428/moviejunkie/model/Account.java
@@ -1,4 +1,77 @@
 package ca.mcgill.ecse428.moviejunkie.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Account {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private int accountId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+
+    public Account(){}
+
+    public Account(int accountId, String firstName, String lastName, String username, String password, String email) {
+        this.accountId = accountId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/main/java/ca/mcgill/ecse428/moviejunkie/repository/AccountRepository.java
+++ src/main/java/ca/mcgill/ecse428/moviejunkie/repository/AccountRepository.java
@@ -1,4 +1,6 @@
 package ca.mcgill.ecse428.moviejunkie.repository;
-
-public class AccountRepository {
+import ca.mcgill.ecse428.moviejunkie.model.Account;
+import org.springframework.data.repository.CrudRepository;
+public interface AccountRepository extends CrudRepository<Account, Integer>{
+    Account findAccountByID(int id);
 }
